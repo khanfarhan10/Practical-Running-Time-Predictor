2ef088e75ea30274120c63e86be09b2f791f888d
CrazyCoderz <njrfarhandasilva10@gmail.com>
2020-01-05 01:10:36 +0530

    Add files via upload

---
+++ Regression Program(OCTAVE-MATLAB)/QuickSort(n^2)/RegressionProgram.m
@@ -0,0 +1,80 @@
+clc
+
+
+
+#load the training dataset
+#data=load('MergeSort.txt');
+#or
+data=load('QuickSortLastPivot.txt');
+
+[row,col]=size(data)
+#extract first column as y practical running time
+y=data(:,1);
+#and subsequent rows as x the features n
+x=data(:,2:end)
+size(x)
+
+
+#extend the features to get a regression fit such as nlogn,n,etc
+
+#for QuickSort
+x = [x,x.*x]
+#for MergeSort/QuickSort
+#x = [x,x.*log2(x)]
+
+
+
+[row,col]=size(x)
+
+#plotting_data_2d
+figure(1)
+plot(x(:,1), y, 'rx', 'MarkerSize', 10) #try experimenting with -xr -ob
+#title('Plot for Algorithm MergeSort');
+title('Plot for Algorithm QuickSort(n^2)');
+ylabel('Time Complexity u(n)'); % Set the y  axis label
+xlabel('Input Size n'); % Set the x  axis label
+legend('label1','label2')
+#plotting data 3d if 3 (or more) variables are present
+if(col>=2)
+  figure(2)
+  scatter3(x(:,1),x(:,2),y)
+  #title('Plot for Algorithm MergeSort');
+  title('Plot for Algorithm QuickSort(n^2)');
+  xlabel('Feature1 n1=n'); % Set the x  axis label
+  ylabel('Feature2 n2=n^2'); % Set the y  axis label
+  zlabel('u(n)'); % Set the z axis label
+endif
+
+
+
+%% ================ Gradient Descent/Optimization ================
+
+
+m = length(y);
+#no of training examples
+x = [ones(m , 1) , x];
+% Add intercept term to X
+[row,col]=size(x)
+theta_0 = [zeros(1,col)]'
+#initial theta is just a column vector of the number of features in x
+
+options = optimset('GradObj' , 'on' , 'MaxIter' , 1500);
+[theta_opt, cost] = fminunc(@ (t) computeCost(x,y,t) , theta_0 , options) 
+
+#plotting the results/regression line obtained
+
+figure(3)
+plot(x(:,2), y, 'rx', 'MarkerSize', 10) #-xr -ob
+
+#title('Plot for Algorithm MergeSort');
+title('Plot for Algorithm QuickSort(n^2)');
+ylabel('Time Complexity u(n)'); % Set the y ? axis label
+xlabel('Input Size n'); % Set the x ? axis label
+#print first column only
+hold on;
+#now plotting the regression line
+h=theta_opt'*x';
+plot(x(:,2), h)
+legend('original data points','regression line')
+hold off;
+
